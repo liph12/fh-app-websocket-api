1d5e009360e598fddeb0367517528a6d78d24615
unknown <libresphilip14@gmail.com>
2024-03-26 13:35:16 +0800

    cors policy modified

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 const port = 3000;
-
-io.origins("*:*");
 
 io.on("connection", (socket) => {
   let room;
